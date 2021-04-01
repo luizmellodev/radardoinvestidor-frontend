c5b85e562a4825653b2ccd8bc2eb140b4c52d3bc
Gabriel Brunichaki <gabriel.brunichaki@triider.com>
2021-04-01 18:52:37 -0300

    screen component: Duda, Gabriel, Martin, Pedro

---
+++ src/components/Screen/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+import { Container } from './styles';
+
+interface ScreenProps {
+  children: ReactNode;
+}
+
+function Screen({ children }: ScreenProps) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+};
+
+export default Screen;
