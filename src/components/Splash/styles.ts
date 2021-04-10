0f4a1e8aaefdcbe0bb9faef462db2ff7a8e3b4a0
Guilherme Piccoli <guilherme.piccoli@acad.pucrs.br>
2021-04-10 17:22:32 +0000

    US01- Splash Screen

---
+++ src/components/Splash/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+background-color: ${(props) => props.theme.colors.primary};
+height: 100%;
+display: flex;
+justify-content: center; 
+align-items: center;
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-
+import { useState, useEffect } from 'react';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+import SplashScreen from '../components/Splash';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Component {...pageProps} />
+      {isLoading ? (
+          <SplashScreen/>
+        ) :
+      <Component {...pageProps} />}
     </ThemeProvider>
   );
 }
