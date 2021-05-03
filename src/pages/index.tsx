3b4a5b2fd11ec8b48339a8c28b779ce09fd43580
Martin Ferreira <martin.ferreira@e-core.com>
2021-05-03 17:47:05 -0300

    Alterar numero de fundos selecionados - Martin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
-
 import SubmitButton from 'components/SubmitButton';
 import Screen from 'components/Screen';
 import HeaderHome from 'components/HeaderHome';
@@ -9,56 +8,46 @@ import FundCard from 'components/FundCard';
 import Loading from 'components/Loading';
 import Tabs from 'components/Tabs';
 import Tab from 'components/Tabs/Tab';
-
 import { FundsContext } from 'contexts/Funds';
-
 export const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
 `;
-
 export const Header = styled.header`
   padding-bottom: 40px;
 `;
-
 export const List = styled.div`
   margin-top: 32px;
 `;
-
 export const Footer = styled.footer`
   padding-top: 24px;
 `;
-
 export default function Home() {
   const router = useRouter();
   const { foundedFunds, filterFundByName, selectedFunds } = useContext(
     FundsContext
   );
-
   const handleCompareButtonClick = () => {
     router.push('/comparacao');
   };
-
   const handleOnChangeText = async (searchText: string) => {
     filterFundByName(searchText);
   };
-
+  const tabSelectedFunds: string =
+    (('Selecionados (' + (selectedFunds.length as any)) as string) + ')';
   const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 3000);
   }, []);
-
   return (
     <Screen>
       <Container>
         <Header>
           <HeaderHome onChangeHandler={handleOnChangeText} />
         </Header>
-
         <Tabs>
           <Tab title="Encontrados">
             {isLoading ? (
@@ -71,19 +60,22 @@ export default function Home() {
               </List>
             )}
           </Tab>
-          <Tab title="Selecionados">
-            <List>
-              {selectedFunds.length ? (
-                selectedFunds.map((fund) => (
+          {selectedFunds.length ? (
+            <Tab title={tabSelectedFunds}>
+              <List>
+                {selectedFunds.map((fund) => (
                   <FundCard fund={fund} key={fund.razaoSocial} />
-                ))
-              ) : (
+                ))}
+              </List>
+            </Tab>
+          ) : (
+            <Tab title="Selecionados">
+              <List>
                 <p>Nenhum Fundo Selecionado</p>
-              )}
-            </List>
-          </Tab>
+              </List>
+            </Tab>
+          )}
         </Tabs>
-
         <Footer>
           <SubmitButton
             isDisable={!selectedFunds.length}
