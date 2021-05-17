fea35487f922a41db24e564ed7ca04a9b219a5ac
Martin Ferreira <martinwdf@gmail.com>
2021-05-17 19:06:16 -0300

    centralizar loading e mensagem de sem fundos selecionados - Martin

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   flex: 1;
   display: flex;
+  height: 100%;
   justify-content: center;
   align-items: center;
   animation: blink 0.75s linear infinite;
